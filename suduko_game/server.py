e0c6da60466a4cf8bacc8bc8eafbbc9f662fba85
pujitha <akshayapujithakolli@gmail.com>
2023-02-27 00:45:10 +0530

    for dynamic content in frontend done

--- suduko_game/server.py
+++ suduko_game/server.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+from validation import *
 
 app=Flask(__name__)
 
@@ -9,11 +10,16 @@ def home():
 
 @app.route("/submit",methods=["POST"])
 def submit():
-    num2=request.form.get("2")
-    num5=request.form.get("5")
-    data={'num2':num2,'num5':num5}
-    print(num2)
-    print(num5)
+    dic={}
+    l=['1','2','3','4','5','6','7','8','9']
+    for i in range(1,83):
+        x="num"+str(i)
+        num=request.form.get(str(i))
+        if num  in l:
+            dic[x]=num
+    #print(dic)
+        
+    data=dic
     return render_template("suduko.html",data=data)
 
 if __name__=="__main__":
